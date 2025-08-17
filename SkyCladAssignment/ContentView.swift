0d64b2ce7f262434ab03124cae002499f7c4ba24
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-08-17 18:53:37 +0530

    Initial Commit

---
+++ SkyCladAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyCladAssignment
+//
+//  Created by A Avinash Chidambaram on 17/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyCladAssignment/SkyCladAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCladAssignmentApp.swift
+//  SkyCladAssignment
+//
+//  Created by A Avinash Chidambaram on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SkyCladAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
